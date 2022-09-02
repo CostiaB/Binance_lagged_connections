89163d02d5950b076a16a84b5ab6961e4395e630
Konstantin Belotelov <33226823+CostiaB@users.noreply.github.com>
2022-09-02 12:32:29 +0300

    added end day

--- binance_download.py
+++ binance_download.py
@@ -1,12 +1,13 @@
 import pandas as pd
 from binance.client import Client
-import datetime as dt
+
 
 def download_data(symbols:list,
                   start_day:str,
                   interval:str,
                   api_key:str,
                   api_secret:str,
+                  end_day=None
                   ):
     '''
     
@@ -15,14 +16,17 @@ def download_data(symbols:list,
     symbols : list[str]
         List of symbol names
     start_day : str
-        first day to download string format (YYYY-MM-DD)
+        first day to download string in UTC format (YYYY-MM-DD)
     interval : str
         type of a candles interval variants: '1m', '3m', '5m', '15m', '30m',
-        '1h', '2h', '4h', '6h', '8h', '12h', '1d', '3d', '1w', '1M'.
+        '1h', '2h', '4h', '6h', '8h', '12h', '1d', '3d', '1w', '1M'
     api_key : str
         binance api key
     api_secret : str
         binance api secret key
+    end_day :str 
+        optional - end date string in UTC format (if not submitted it will
+                                                  fetch everything up to now)
     
     
     Returns
@@ -39,7 +43,11 @@ def download_data(symbols:list,
  
     for s in symbols:
         print(f'Downloading {s} data')
-        klines = client.get_historical_klines(s, interval, start_day)
+        if end_day:
+            klines = client.get_historical_klines(s, interval, start_day,
+                                                  end_str=end_day)
+        else:
+            klines = client.get_historical_klines(s, interval, start_day)
         tmp = pd.DataFrame(klines, columns=columns)
         tmp.loc[:, 'Ticker_Full_Name'] = s
         data = pd.concat([data, tmp])
